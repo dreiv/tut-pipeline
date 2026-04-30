2a584452bf604b2a2ac68d05283af7cc4715d452
Voicu Andrei <dreivoicu@gmail.com>
2026-04-30 13:39:53 +0300

    support for both unit and browser tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,36 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from "node:url";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      root: fileURLToPath(new URL("./", import.meta.url)),
+
+      projects: [
+        mergeConfig(viteConfig, {
+          test: {
+            name: "unit",
+            environment: "node",
+            include: ["src/**/*.unit.{test,spec}.{js,ts}"],
+            exclude: [...configDefaults.exclude, "e2e/**"],
+          },
+        }),
+        mergeConfig(viteConfig, {
+          test: {
+            name: "browser",
+            include: ["src/**/*.browser.{test,spec}.{js,ts}"],
+            browser: {
+              enabled: true,
+              provider: playwright(),
+              instances: [{ browser: "chromium" }],
+              headless: true,
+            },
+          },
+        }),
+      ],
     },
   }),
-)
+);
